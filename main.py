6269ff520cb50fbd15813df6a5262ab319cc3a45
HWaiChan <hanswai93@gmail.com>
2022-04-12 22:21:25 +0100

    Original

---
+++ main.py
@@ -0,0 +1,81 @@
+# bot.py
+import os
+import youtube_dl
+
+
+import discord
+from dotenv import load_dotenv
+
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = commands.Bot(command_prefix="!")
+
+
+@client.command()
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wat")
+        return
+
+    print(f'{client.user} has connected to Discord!')
+    voice_channel = discord.utils.get(ctx.guild.voice_channels, name="General")
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if not voice or not voice.is_connected():
+        await voice_channel.connect()
+    
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        print("Disconnect not called.")
+
+
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_playing():
+        voice.pause()
+    else:
+        print("Pause not called.")
+
+
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_paused():
+        voice.resume()
+    else:
+        print("Resume not called.")
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+client.run(TOKEN)
